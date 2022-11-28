3dba2740bd80e273e349a5a8031e3e8de1709380
laurelcrelia <laurel.matt@helsinki.fi>
2022-11-28 23:10:25 +0200

    1. ostoskorin hinta ja tavaroiden määrä oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,10 +5,18 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.tuote1 = Tuote("banaani",1)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
+        self.setUp()
         self.assertEqual(self.kori.hinta(), 0)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+    # def test_yksi_tuote_lisataan_oikein(self):
+    #     self.setUp()
+    #     self.kori.lisaa_tuote(self.tuote1)
+    #     saldo = self.kori.tavaroita_korissa()
+    #     self.assertEqual(saldo, 1)
 
     

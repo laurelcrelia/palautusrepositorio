21973e0d3cdcee8d19479f51c54e6c9d6cebc11d
laurelcrelia <laurel.matt@helsinki.fi>
2022-11-28 21:47:24 +0200

    tehtava 2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,21 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lisaa_kortille_jos_ladattava_summa_on_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+        
+        self.kassa.lataa(maksukortti_mock, 10)
+    
+
+        maksukortti_mock.lataa.assert_called_with(10)
+
+    def test_ei_tee_mitaan_jos_ladattava_summa_on_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+        
+        self.kassa.lataa(maksukortti_mock, -10)
+    
+
+        maksukortti_mock.lataa.assert_not_called()

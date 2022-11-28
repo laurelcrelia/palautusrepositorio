a647bce4e23ed80f62be934668590ad87ba736e5
laurelcrelia <laurel.matt@helsinki.fi>
2022-11-28 22:45:09 +0200

    tehtävä 4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -94,4 +94,45 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.tilimaksu("pekka", "12345")
 
         self.pankki_mock.tilisiirto.assert_called_with("pekka",ANY,"12345","33333-44455",6)
+
+    def test_aloita_asiointi_nollaa_edelliset(self):
+        self.setUp()
+
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("jaska", "34567")
+
+        self.pankki_mock.tilisiirto.assert_called_with("jaska",ANY,"34567","33333-44455",5)
+
+    def test_pyytaa_aina_uuden_viitenumeron(self):
+        self.setUp()
+
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 1)
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("jaska", "34567")
+
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
+
+    def test_tuotteen_poistaminen(self):
+        self.setUp()
+
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.poista_korista(1)
+        self.kauppa.tilimaksu("jaska", "34567")
+
+        self.pankki_mock.tilisiirto.assert_called_with("jaska",ANY,"34567","33333-44455",6)
         

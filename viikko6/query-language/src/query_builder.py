07b631737db02d0bba3b1dd7874ea295b7e4daed
laurelcrelia <laurel.matt@helsinki.fi>
2022-12-12 21:39:17 +0200

    tehtävä 4

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,17 @@
+from matchers import And, PlaysIn, HasAtLeast, All, HasFewerThan, Or
+
+class QueryBuilder:
+    def __init__(self, query_builder = All()):
+        self.query_builder = query_builder
+
+    def build(self):
+        return self.query_builder
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self.query_builder, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.query_builder, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.query_builder, HasFewerThan(value, attr)))
